7263a59b274d2420295b9ff1f3865fead67819b0
Bektemur Mamashaev <104037405+bektemur-07@users.noreply.github.com>
2023-01-31 13:37:46 +0500

    Add cell swipe action to the NotesTableViewController

--- NotesApp/NotesTableViewController.swift
+++ NotesApp/NotesTableViewController.swift
@@ -68,5 +68,35 @@ class NotesTableViewController: UITableViewController {
         objects.insert(movedEmoji, at: destinationIndexPath.row)
         tableView.reloadData()
     }
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let done = doneAction(at: indexPath)
+        let love = favouriteAction(at: indexPath)
+        return UISwipeActionsConfiguration(actions: [love, done])
+    }
+    
+    
+    func doneAction(at indexPath: IndexPath) -> UIContextualAction {
+        let action = UIContextualAction(style: .destructive, title: "Done") { (action, view, completion) in
+            self.objects.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            completion (true)
+        }
+        action.backgroundColor = .systemGreen
+        action.image = UIImage(systemName: "checkmark.circle")
+        return action
+    }
+    
+    
+    func favouriteAction(at indexPath: IndexPath) -> UIContextualAction {
+        var object = objects[indexPath.row]
+        let action  = UIContextualAction(style: .normal, title: "Like") { (action, view, completion) in
+            object.isFavourite = !object.isFavourite
+            self.objects[indexPath.row] = object
+            completion (true)
+        }
+        action.backgroundColor = object.isFavourite ? .systemPurple : .systemGray
+        action.image = UIImage(systemName: "heart")
+        return action
+    }
 
 }
