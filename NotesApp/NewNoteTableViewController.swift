35365c60f8c9b608d0e9f4ec3846a4e0cf118770
Bektemur Mamashaev <104037405+bektemur-07@users.noreply.github.com>
2023-02-01 17:07:48 +0500

    work with saveButton

--- NotesApp/NewNoteTableViewController.swift
+++ NotesApp/NewNoteTableViewController.swift
@@ -24,23 +24,24 @@ class NewNoteTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    private func updateSaveButtonState() {
+        let emojiText = emojiTextField.text ?? ""
+        let nameText = nameTextField.text ?? ""
+        let descriptionText = descriptionTextField.text ?? ""
+        saveButton.isEnabled = !emojiText.isEmpty && !nameText.isEmpty && !descriptionText.isEmpty
+    }
+    
+    
     @IBAction func textChanged(_ sender: UITextField) {
-        
+        updateSaveButtonState()
     }
     
     
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
 
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
